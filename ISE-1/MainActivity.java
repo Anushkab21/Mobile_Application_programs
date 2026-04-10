8bbb4ce47731bd9b5d7216af7762a463ddc8f50d
Anushka Bawkar <anushkabawkar2110@gmail.com>
2026-04-10 16:10:19 +0530

    Add MainActivity for app entry point

---
+++ ISE-1/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.poe;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+Button b1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        b1=findViewById(R.id.welcome);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i =new Intent(MainActivity.this,homepage.class);
+                startActivity(i);
+            }
+        });
+    }
+}
