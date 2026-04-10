721bcf4b0c09c343aecd12c347add892775ac9f2
Anushka Bawkar <anushkabawkar2110@gmail.com>
2026-04-10 16:15:54 +0530

    Create lab3.java

---
+++ ISE-1/lab3.java
@@ -0,0 +1,31 @@
+package com.example.poe;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class lab3 extends AppCompatActivity {
+    Button b1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.lab2);
+        b1=findViewById(R.id.button);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i =new Intent(lab3.this,homepage.class);
+                startActivity(i);
+            }
+        });
+    }
+}
